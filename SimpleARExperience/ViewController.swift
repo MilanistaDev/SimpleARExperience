fd78675f3dfff74fe92acf6384d61572472446b3
Takuya Aso <milanista224@gmail.com>
2018-09-18 21:29:04 +0900

    Initial Commit

---
+++ SimpleARExperience/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleARExperience
+//
+//  Created by 麻生 拓弥 on 2018/09/18.
+//  Copyright © 2018年 com.ASTK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
