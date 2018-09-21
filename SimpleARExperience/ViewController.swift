5539d937accb8f09a4066e9c114684f4d30ea60f
Takuya Aso <milanista224@gmail.com>
2018-09-22 01:31:12 +0900

    Implemented Quick Look side

--- SimpleARExperience/ViewController.swift
+++ SimpleARExperience/ViewController.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import QuickLook
 
 final class ViewController: UIViewController {
 
     @IBOutlet weak var contentsCollectionView: UICollectionView!
 
     // USDZファイル名のArray
-    private let contentsArray: [String] = ["cupandsaucer", "gramophone", "plantpot",
+    fileprivate let contentsArray: [String] = ["cupandsaucer", "gramophone", "plantpot",
                                            "redchair", "retrotv", "teapot", "trowel",
                                            "tulip", "wateringcan", "wheelbarrow"]
+    // セルを押した際のCollectionViewのindex値
+    fileprivate var selectedItemIndex: Int = -1
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,11 +32,29 @@ final class ViewController: UIViewController {
         let cellNib = UINib(nibName: "ContentsCollectionViewCell", bundle: nil)
         self.contentsCollectionView.register(cellNib, forCellWithReuseIdentifier: "ContentsCell")
     }
+
+    fileprivate func previewARContents() {
+        let previewController = QLPreviewController()
+        previewController.dataSource = self
+        self.present(previewController, animated: true, completion: nil)
+    }
 }
+
+// MARK: - UICollectionViewDelegate
 
 extension ViewController: UICollectionViewDelegate {
 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // CollectionViewのセルをタップしたindex値を保持しておく
+        self.selectedItemIndex = indexPath.row
+        // QLPreviewControllerを開く
+        let previewController = QLPreviewController()
+        previewController.dataSource = self
+        self.present(previewController, animated: true, completion: nil)
+    }
 }
+
+// MARK: - UICollectionViewDataSource
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -47,7 +68,7 @@ extension ViewController: UICollectionViewDataSource {
     }
 }
 
-// MARK:- UICollectionViewDelegateFlowLayout
+// MARK: - UICollectionViewDelegateFlowLayout
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
 
@@ -55,5 +76,32 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
         let width: CGFloat = UIScreen.main.bounds.width / 2 - 7.5
         let height = width
         return CGSize(width: width, height: height)
+    }
+}
+
+// MARK:- QLPreviewController DataSource
+
+extension ViewController: QLPreviewControllerDataSource {
+
+    /// Quick Look で表示するアイテム数
+    ///
+    /// - Parameter controller: controller
+    /// - Returns: Quick Look で表示するアイテム数
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return 1
+    }
+
+    /// Quick Look で表示するアイテムを返す
+    ///
+    /// - Parameters:
+    ///   - controller: controller
+    ///   - index: アイテムのindex値
+    /// - Returns: Quick Look で表示するアイテム
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        // Return the file URL to the .usdz file
+        let fileUrl = (self.selectedItemIndex < 0) ?
+            Bundle.main.url(forResource: "cupandsaucer", withExtension: "usdz")! :
+            Bundle.main.url(forResource: self.contentsArray[self.selectedItemIndex], withExtension: "usdz")!
+        return fileUrl as QLPreviewItem
     }
 }
